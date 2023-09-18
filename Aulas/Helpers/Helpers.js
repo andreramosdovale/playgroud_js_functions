75d1f8a3ac0fc2b45e845188ab16dce74890a0e2
André Ramos <andre_ramosdovale@outlook.com>
2023-09-18 16:50:03 -0300

    feat: Criação de funções helpers

---
+++ Aulas/Helpers/Helpers.js
@@ -0,0 +1,13 @@
+const bomDia = () => {
+  console.log('Bom dia!');
+};
+
+const boaTarde = () => {
+  console.log('Boa tarde!');
+};
+
+const boaNoite = () => {
+  console.log('Boa noite!');
+};
+
+export {bomDia, boaTarde, boaNoite};
